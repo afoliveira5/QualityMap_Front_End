581c584f0ba69db037c463b667e75f8a6bc381ef
afoliveira5 <aparecidoferreiradeoliveira@gmail.com>
2024-03-23 00:36:59 -0300

    Subindo a estrutura

---
+++ README.md
@@ -0,0 +1,22 @@
+# Welcome to the Quality repository of the project
+## Cypress_with_cucumber + Gherkin 
+
+### In this document I will teach you how to install the project step by step and how to run the project too
+
+let's go ?
+
+#### Step 1: clone the repository on your machine
+
+>```
+>git clone https://github.com/afoliveira5/estudando_cypress_com_cucumber_gherkin.git
+>```
+
+#### Step 2 : Now let's install the dependencies with the command below
+>```
+>npm install
+>```
+
+#### Step 3 : To run the project use the command below
+>```
+>npm run test
+>```
---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity: true,
+  experimentalModifyObstructiveThirdPartyCode: true,
+  experimentalSessionSupport:true,
+  e2e: {
+   baseUrl:"https://demo.nopcommerce.com/",
+   chromeWebSecurity: false,
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: "**/*.feature",
+   // defaultCommandTimeout: 120000,
+    // testIsolation: true,
+    experimentalModifyObstructiveThirdPartyCode: true,
+    defaultCommandTimeout: 10000,
+    experimentalSessionSupport:true,
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    //experimentalWebKitSupport:true,
+  },
+  retries:{
+    //"runMode":2,
+    //"openMode":2
+  }
+});
